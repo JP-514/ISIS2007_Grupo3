828345c25b6e5f41b87a0e1f6392bee938265f7b
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2023-05-09 22:07:45 -0500

    works_pero muy lento

--- data_collect.py
+++ data_collect.py
@@ -6,18 +6,7 @@ import numpy as np
 video = cv2.VideoCapture(0)
 facedetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 count = 0 
-known_face_encodings = [
-    
-]
-known_face_names = [
-    
-]
 
-
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
 
 def register_someone():
 
@@ -52,54 +41,63 @@ def video_start():
 
 
 #####
+video = cv2.VideoCapture(0)
+
+obama_image = face_recognition.load_image_file("./Imagenes/President_Barack_Obama.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+known_face_names = ["Barack Obama"]
+known_face_encodings = [
+    obama_face_encoding
+]
+known_face_names = [
+    "Barack Obama"
+]
 
 while True:
-        ret, frame =video.read()
-        if process_this_frame:
+    # Grab a single frame of video
+    ret, frame = video.read()
 
-            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_frame = frame[:, :, ::-1]
 
-            rgb_small_frame = small_frame[:, :, ::-1]
+    # Find all the faces and face enqcodings in the frame of video
+    face_locations = face_recognition.face_locations(rgb_frame,model="hog")
+    face_encodings = face_recognition.face_encodings(frame)
 
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+    # Loop through each face in this frame of video
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
 
-        
-            face_names = []
-            for face_encoding in face_encodings:
-                matches = face_recognition.compare_faces(known_face_encodings,face_encoding)
-                name ="unknown"
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
+        name = "Unknown"
 
-                face_names.append(name)
+        # If a match was found in known_face_encodings, just use the first one.
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_face_names[first_match_index]
 
-        process_this_frame = not process_this_frame
+        # Or instead, use the known face with the smallest distance to the new face
+        #face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        #best_match_index = np.argmin(face_distances)
+        #if matches[best_match_index]:
+        #    name = known_face_names[best_match_index]
 
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-                # Draw a box around the face
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+    # Display the resulting image
+    cv2.imshow('Video', frame)
 
-                # Draw a label with a name below the face
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-
-        cv2.imshow("Video",frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-video.realease()
-cv2.destroyAllWindows
-
-
+# Release handle to the webcam
+video.release()
+cv2.destroyAllWindows()
