75968fe1a79adbe13e8f2c44d05d615e276f1c04
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2023-05-09 22:38:45 -0500

    ___mejora en video

--- data_collect.py
+++ data_collect.py
@@ -54,6 +54,8 @@ known_face_names = [
     "Barack Obama"
 ]
 
+
+count_fps = 0;
 while True:
     # Grab a single frame of video
     ret, frame = video.read()
@@ -62,8 +64,10 @@ while True:
     rgb_frame = frame[:, :, ::-1]
 
     # Find all the faces and face enqcodings in the frame of video
-    face_locations = face_recognition.face_locations(rgb_frame,model="hog")
-    face_encodings = face_recognition.face_encodings(frame)
+    if (count_fps%30 ==0 ):
+        face_locations = face_recognition.face_locations(rgb_frame,model="hog")
+        face_encodings = face_recognition.face_encodings(frame,face_locations)
+    count_fps+=1
 
     # Loop through each face in this frame of video
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
@@ -77,20 +81,12 @@ while True:
             first_match_index = matches.index(True)
             name = known_face_names[first_match_index]
 
-        # Or instead, use the known face with the smallest distance to the new face
-        #face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-        #best_match_index = np.argmin(face_distances)
-        #if matches[best_match_index]:
-        #    name = known_face_names[best_match_index]
-
-        # Draw a box around the face
+  
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
     # Display the resulting image
     cv2.imshow('Video', frame)
 
